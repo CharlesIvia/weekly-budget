18bb4105ae09a8be2d445bc3be83d2b86ca2b39c
Charles Ivia <iviacharles@gmail.com>
2020-06-09 14:44:33 +0300

    Fetched dom elements and set variables

---
+++ script.js
@@ -0,0 +1,11 @@
+//Fetch dom elements and set variables
+
+const enterBudget = document.querySelector("#enter-budget");
+const firstForm = document.querySelector("#first-form");
+const expenseName = document.querySelector("#name");
+const expenseAmount = document.querySelector("#amount");
+const secondForm = document.querySelector("#second-form");
+const myBudget = document.querySelector("#budget-info");
+const amountLeft = document.querySelector("#budget-info-two");
+const expenseList = document.querySelector("#list");
+let budget;
